dec06889330f7b03dc304e8ee7a74eac7e1ae176
wronah <hubertwro7@gmail.com>
2025-03-31 21:39:52 +0200

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-// Console.Write("$ ");
+Console.Write("$ ");
 
 // Wait for user input
 Console.ReadLine();
