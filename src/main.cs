150284a5c042bc8bad59762ffa7c19969b582a73
wronah <hubertwro7@gmail.com>
2025-03-31 22:36:08 +0200

    implement type builtin

--- src/main.cs
+++ src/main.cs
@@ -1,24 +1,36 @@
 using System.Net;
 using System.Net.Sockets;
 
-while(true)
+while (true)
 {
     Console.Write("$ ");
 
     // Wait for user input
     var command = Console.ReadLine().Split(' ');
+    var builtinCommands = new List<string> { "exit", "echo", "type" };
 
-    if (command[0] == "exit" && command[1] == "0")
+    if (command[0] == builtinCommands[0] && command[1] == "0")
     {
         break;
     }
-    else if (command[0] == "echo")
+    else if (command[0] == builtinCommands[1])
     {
         for (int i = 1;  i < command.Length; i++)
         {
             Console.Write($"{command[i]} ");
         }
         Console.WriteLine();
+    }
+    else if (command[0] == builtinCommands[2])
+    {
+        if(builtinCommands.Contains(command[1]))
+        {
+            Console.WriteLine($"{command[1]} is a shell builtin");
+        }
+        else
+        {
+            Console.WriteLine($"{command[1]}: not found");
+        }
     }
     else
     {
