6fafe588fb9c5c7e635d1873bc01796f505d2cc1
wronah <hubertwro7@gmail.com>
2025-03-31 21:50:38 +0200

    implement exit

--- src/main.cs
+++ src/main.cs
@@ -8,5 +8,10 @@ while(true)
     // Wait for user input
     var command = Console.ReadLine();
 
+    if(command == "exit 0")
+    {
+        break;
+    }
+
     Console.WriteLine($"{command}: command not found");
 }
